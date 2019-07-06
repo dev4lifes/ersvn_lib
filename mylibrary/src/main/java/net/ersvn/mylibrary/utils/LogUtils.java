591ab28f643a1a9aed751683ca12936a1425a5b2
minh_desktop <tuanminhxd1@gmail.com>
2019-07-06 22:49:18 +0700

    init my library

---
+++ mylibrary/src/main/java/net/ersvn/mylibrary/utils/LogUtils.java
@@ -0,0 +1,12 @@
+package net.ersvn.mylibrary.utils;
+
+import android.util.Log;
+
+public class LogUtils {
+
+    private static final String TAG = "ersvn.net";
+
+    public static void info(String content){
+        Log.i(TAG,content);
+    }
+}
